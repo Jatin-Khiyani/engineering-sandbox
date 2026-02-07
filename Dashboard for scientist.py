a67cc6068311639b2fe36f7f40ebd720f05a738c
Jatin-Khiyani <jatinkhiyani14@gmail.com>
2026-02-07 20:03:46 +0100

    Removed Import of Streamlitfrom Dashbaord for scientist.py file, it doesn't require it only Front End Streamlit.py requires it

--- Dashboard for scientist.py
+++ Dashboard for scientist.py
@@ -1,10 +1,8 @@
 import numpy as np
 import pandas as pd
-import streamlit as st
 
 Diabetes_df = pd.read_csv("diabetes copy.csv")
 print(f" The first five entries are: \n{Diabetes_df.head()}")
-
 print(f"Number of Rows: \n{Diabetes_df.shape[0]}")
 print(f"Number of Columns: \n{Diabetes_df.shape[1]}")
 print(f"Overall Shape: \n{Diabetes_df.shape}")
