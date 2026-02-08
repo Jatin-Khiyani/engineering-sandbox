bd49b808ef5b1a653baa6088233cf55599aa755b
Jatin-Khiyani <jatinkhiyani14@gmail.com>
2026-02-08 22:03:26 +0100

    Added Additional Features for the Front End Streamlit.py, Now the Dashboard has Statistics, Boxplot for outlier Detection, Correlation Matrix and its Visualization and Histogram for Column Distribution Visulization

--- Front End Streamlit.py
+++ Front End Streamlit.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.title("Dashboard for Scientist")
 CSV_data = st.file_uploader("Upload CSV File", type=["csv"])
@@ -22,3 +24,35 @@ st.text("Total NUll Values: ")
 st.write(Diabetes_df.isnull().sum().sum())
 st.text("Column Wise Null Values Are: ")
 st.dataframe(Diabetes_df.isnull().sum())
+st.text("Basic Statistics for Numerical Colums: ")
+st.dataframe(Diabetes_df.describe())
+
+st.text("Box Plot for Outlier Detection: Enter Column Name")
+
+column_for_outlier_detection = st.text_input("Enter column name")
+
+if column_for_outlier_detection:  # only runs if not empty
+    if column_for_outlier_detection in Diabetes_df.columns:
+        fig, ax = plt.subplots()
+        sns.boxenplot(Diabetes_df[column_for_outlier_detection])
+        st.pyplot(fig)
+    else:
+        st.warning("❌ Column not found. Please enter a valid column name.")
+else:
+    st.info("👆 Please enter a column name to generate the boxplot.")
+st.text("Correlations on Your dataset: ")
+st.dataframe(Diabetes_df.corr())
+fig2, ax = plt.subplots()
+sns.heatmap(Diabetes_df.corr(), cmap="YlGnBu", annot=True)
+st.pyplot(fig2)
+st.text("Historgam for Distribtution: Enter Column Name")
+histogram_Column = st.text_input("Enter Column Name for Histogram Visualization")
+if histogram_Column:  # only runs if not empty
+    if histogram_Column in Diabetes_df.columns:
+        fig3, ax = plt.subplots()
+        hist = Diabetes_df[histogram_Column].hist(bins=8)
+        st.pyplot(fig3)
+    else:
+        st.warning("❌ Column not found. Please enter a valid column name.")
+else:
+    st.info("👆 Please enter a column name to generate the Histogram.")
